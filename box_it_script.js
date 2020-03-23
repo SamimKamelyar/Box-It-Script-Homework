86d4ebfa37035d1944907e25e690b54537a2393c
Samim Kamleyar <ahmadsamimkamelyar@gmail.com>
2020-03-22 19:59:04 -0700

    Functions completed and got the final output

--- box_it_script.js
+++ box_it_script.js
@@ -1,5 +1,5 @@
 // Creating an array with our Users names.
-let users = ['Jon Snow', 'Cersei Lannister', 'Daenerys Targaryen'];
+let users = ['Jon Snow          ', 'Cersei Lannister  ', 'Daenerys Targaryen'];
 
 //For loop will loop them to the eng and print them one by one.
 // for (let i = 0; i < users.length; i++) {
@@ -15,58 +15,39 @@ function drawline(n) {
   return line;
 }
 
+//This function will draw Side line before and after our String.
+function drawbars(string) {
+  return '\u{2503}' + string + '\u{2503}'
+}
+
 // This function will draw Top line on our input.
 function drawlineTop(n) {
   //for (let i = 0;)
-    return '\u{250F}' + drawline(n) + '\u{2513}'
+  return '\u{250F}' + drawline(n) + '\u{2513}'
 }
-console.log(drawlineTop(10));
-
-
+console.log(drawlineTop(18));
+// Printint first element of our array in 0 index.
+console.log(drawbars(users[0]));
 
 
 // This function will draw Middle line in between our inputs.
 function drawlineMiddle(n) {
   return '\u{2523}' + drawline(n) + '\u{252B}'
 }
-console.log(drawlineMiddle(10));
+console.log(drawlineMiddle(18));
+//printing second element of our array in 1 index.
+console.log(drawbars(users[1]));
+
+// This function will draw Middle line in between our inputs.
+function drawlineMiddle(n) {
+  return '\u{2523}' + drawline(n) + '\u{252B}'
+}
+console.log(drawlineMiddle(18));
+//printing second element of our array in 2 index.
+console.log(drawbars(users[2]));
 
 // This function will draw Botom line under our input.
 function drawlineBotom(n) {
   return '\u{2517}' + drawline(n) + '\u{251B}'
 }
-console.log(drawlineBotom(10));
-
-
-
-
-
-// This function will draw Side line before and after our String.
-// function drawbars(string) {
-//   return '\u{2503}' + string + '\u{2503}'
-// }
-// console.log(drawbars());
-
-
-//console.log(drawbars(users));
-
-//console.log(drawline(10));
-
-// for: -
-
-// if (i = 0)
-
-//   Top Border
-
-// arr[i]
-
-// drawlineMiddle
-
-// if (i = arr.length)
-//   arr[i]
-// bottomborder
-
-
-
-
-// console.log(drawlineMiddle(10))
+console.log(drawlineBotom(18));
